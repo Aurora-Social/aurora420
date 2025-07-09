4c060fed3b0b1ebd8e1a8830673d48f7967ad1ac
G-Fenix <hello@g-fenix.com>
2025-07-09 12:10:37 +0200

    use env variables for secret, debug and allowed_hosts

--- Aurora/settings.py
+++ Aurora/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-baxkxeoakow73zx&omb3uh!_r)r3h!lr7ep349l4@)f!)cp%%l'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
