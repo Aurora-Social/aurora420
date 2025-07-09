837765e474ccfcac9946286015f8fe2bef5b7e46
G-Fenix <hello@g-fenix.com>
2025-07-09 12:13:44 +0200

    settings kaydedildi

--- Aurora/settings.py
+++ Aurora/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['aurora420.onrender.com', 'www.aurora420.onrender.com']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
